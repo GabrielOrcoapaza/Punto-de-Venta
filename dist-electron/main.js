1c72b4ec1a9495072b0d3fceb5939c3ff7533263
gabriel carlos <gndark123@gmail.com>
2025-12-10 10:15:52 -0500

    cambios

--- dist-electron/main.js
+++ dist-electron/main.js
@@ -1,44 +1,10 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || (function () {
-    var ownKeys = function(o) {
-        ownKeys = Object.getOwnPropertyNames || function (o) {
-            var ar = [];
-            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
-            return ar;
-        };
-        return ownKeys(o);
-    };
-    return function (mod) {
-        if (mod && mod.__esModule) return mod;
-        var result = {};
-        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
-        __setModuleDefault(result, mod);
-        return result;
-    };
-})();
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const electron_1 = require("electron");
 const path_1 = __importDefault(require("path"));
-const url = __importStar(require("url"));
 const isDev = !electron_1.app.isPackaged;
 let mainWindow = null;
 function createWindow() {
@@ -51,6 +17,7 @@ function createWindow() {
         },
     });
     if (isDev) {
+        // Modo desarrollo: cargar desde Vite
         mainWindow.loadURL('http://localhost:5173');
         mainWindow.webContents.on('before-input-event', (event, input) => {
             if (input.type === 'keyDown' && input.key === 'F12') {
@@ -59,14 +26,10 @@ function createWindow() {
         });
     }
     else {
-        const indexPath = path_1.default.join(__dirname, '..', 'dist', 'index.html'); // Subimos un nivel
-        mainWindow.loadURL(url.format({
-            pathname: indexPath,
-            protocol: 'file:',
-            slashes: true,
-        }));
+        // Modo producción: cargar desde archivos empaquetados
+        const indexPath = path_1.default.join(__dirname, '../dist/index.html');
+        mainWindow.loadFile(indexPath);
     }
-    // Asegura que la ventana tenga foco cuando se crea
     mainWindow.on('ready-to-show', () => {
         mainWindow?.show();
         mainWindow?.focus();
@@ -77,8 +40,9 @@ function createWindow() {
 }
 electron_1.app.whenReady().then(createWindow);
 electron_1.app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin')
+    if (process.platform !== 'darwin') {
         electron_1.app.quit();
+    }
 });
 electron_1.app.on('activate', () => {
     if (electron_1.BrowserWindow.getAllWindows().length === 0) {
